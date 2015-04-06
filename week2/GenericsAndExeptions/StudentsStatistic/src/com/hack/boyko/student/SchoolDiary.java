2309e5b7bedec0a1256bd0696e136fe767f0e26a
boyskila <bmlalov@abv.bg>
2015-04-06 18:31:46 +0300

    Changing dir name

---
+++ week2/GenericsAndExeptions/StudentsStatistic/src/com/hack/boyko/student/SchoolDiary.java
@@ -0,0 +1,35 @@
+package com.hack.boyko.student;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class SchoolDiary {
+
+    private Map<Students, Double> sortedDiary;
+    private Map<Students, Double> diary;
+    private ValueComparator vc;
+
+    public SchoolDiary() {
+        diary = new HashMap<Students, Double>();
+        vc = new ValueComparator(diary);
+        sortedDiary = new TreeMap<Students, Double>(vc);
+    }
+
+    public void addStudent(Students student) {
+        double avarage = student.averageGrade();
+        diary.put(student, avarage);
+        sortedDiary.put(student, avarage);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Entry<Students, Double> student : sortedDiary.entrySet()) {
+            sb.append(student.getKey() + "\n");
+        }
+        return sb.toString();
+    }
+}
