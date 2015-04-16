77a5ee9b6877a6f62bc1f4d5c91a9e7091cf9208
boyskila <bmlalov@abv.bg>
2015-04-16 12:10:09 +0300

    Homework for week3

--- week3/Week3FixSubtitles/src/FixSubtitlesMain.java
+++ week3/Week3FixSubtitles/src/FixSubtitlesMain.java
@@ -3,26 +3,23 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class FixSubtitlesMain {
 
-    public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(
-                "C:/Users/Bojko/Desktop/lost.s04e11.hdtv.xvid-2hd.srt"), "windows-1251"))) {
+        File file = new File("lost.s04e11.hdtv.xvid-2hd.srt");
 
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:/Users/Bojko/Desktop/lostSubs.txt"))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(file), "windows-1251"))) {
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter("lostSubs.txt"))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-System.out.println(line);
+                    System.out.println(line);
                     bw.write(line);
                 }
             }
